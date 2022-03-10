ec93130d30799ad5be295984ae69722f18dc882e
grafica <paolo@monema.it>
2022-03-10 20:32:26 +0100

    home e dati

--- src/components/headerHome.jsx
+++ src/components/headerHome.jsx
@@ -1,16 +1,66 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { useStaticQuery, graphql } from "gatsby"
+import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const HeaderHome = () => {
+    const data = useStaticQuery(graphql`
+      {
+        allDataYaml {
+          edges {
+            node {
+              skills {
+                title
+                apps {
+                  link
+                  name
+                }
+              }
+              personal {
+                logo {
+                  childImageSharp {
+                    gatsbyImageData(width: 300)
+                  }
+                }
+                titles
+              }
+            }
+          }
+        }
+      }
+    `)
 
+
+    const skills = data.allDataYaml.edges[0].node.skills
+    const personal = data.allDataYaml.edges[0].node.personal
+    const logo = getImage(personal.logo)
+    const logos = skills.map((item) => {
+       const y = item.apps.map((i)=>i.logo)
+        return y
+    })
+    console.log(logos);
     return <header>
         <div className="block-home">
+            <div className="logo">
+                <GatsbyImage image={logo} alt={personal.altlogo} />
+                <ul>
+                    {personal.titles.map((item) => {
+                        return <li>{item}</li>
+                    })}
+                </ul>
+            </div>
             <ul className="skills">
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                {skills.map((item) => {
+                    return <li key={item.title}>
+                        {item.title}
+                        <ul>
+
+                            {item.apps.map((item) => {
+                                return <li>{item.name}</li>
+
+                            })}
+                        </ul>
+                    </li>
+                })}
             </ul>
             <ul className="apps">
                 <li></li>
