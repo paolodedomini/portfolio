71f3003ca48c2aeb5e9f721d14df9ad08beb0080
paolo <paolo@monema.it>
2022-03-10 22:56:44 +0100

    dati header

--- src/components/headerHome.jsx
+++ src/components/headerHome.jsx
@@ -3,7 +3,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const HeaderHome = () => {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
       {
         allDataYaml {
           edges {
@@ -30,60 +30,61 @@ const HeaderHome = () => {
     `)
 
 
-    const skills = data.allDataYaml.edges[0].node.skills
-    const personal = data.allDataYaml.edges[0].node.personal
-    const logo = getImage(personal.logo)
-    const logos = skills.map((item) => {
-       const y = item.apps.map((i)=>i.logo)
-        return y
-    })
-    console.log(logos);
-    return <header>
-        <div className="block-home">
-            <div className="logo">
-                <GatsbyImage image={logo} alt={personal.altlogo} />
-                <ul>
-                    {personal.titles.map((item) => {
-                        return <li>{item}</li>
-                    })}
-                </ul>
-            </div>
-            <ul className="skills">
-                {skills.map((item) => {
-                    return <li key={item.title}>
-                        {item.title}
-                        <ul>
+  const skills = data.allDataYaml.edges[0].node.skills
+  const personal = data.allDataYaml.edges[0].node.personal
+  const logo = getImage(personal.logo)
+  const y = []
+  skills.forEach((item) => {
+    
+    item.apps.forEach((item) => y.push(item))
+  })
+  
+  return <header>
+    <div className="block-home">
+      <div className="logo">
+        <GatsbyImage image={logo} alt={personal.altlogo} />
+        <ul>
+          {personal.titles.map((item) => {
+            return <li>{item}</li>
+          })}
+        </ul>
+      </div>
+      <ul className="skills">
+        {skills.map((item) => {
+          return <li key={item.title}>
+            {item.title}
+            <ul>
 
-                            {item.apps.map((item) => {
-                                return <li>{item.name}</li>
+              {item.apps.map((item) => {
+                return <li>{item.name}</li>
 
-                            })}
-                        </ul>
-                    </li>
-                })}
+              })}
             </ul>
-            <ul className="apps">
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-        </div>
-        <StaticImage
-            src="../images/back-home.jpg"
-            width={1728}
-            quality={95}
-            formats={["auto", "webp", "avif"]}
-            alt="Immagine di sfondo home"
-            className="immagine-home"
-        />
-    </header>
+          </li>
+        })}
+      </ul>
+      <ul className="apps">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+    <StaticImage
+      src="../images/back-home.jpg"
+      width={1728}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="Immagine di sfondo home"
+      className="immagine-home"
+    />
+  </header>
 
 }
 
