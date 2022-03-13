a78cbf49008f3c7df1119b4a7df2b24930a1f536
grafica <paolo@monema.it>
2022-03-13 16:25:45 +0100

    animazioni header

--- src/components/headerHome.jsx
+++ src/components/headerHome.jsx
@@ -1,17 +1,26 @@
 import * as React from "react"
+import { useRef } from 'react'
 import { useStaticQuery, graphql } from "gatsby"
-import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { motion, AnimatePresence } from "framer-motion"
 
 import Scritte from "./scritte"
 
 const HeaderHome = () => {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
       {
         allDataYaml {
           edges {
             node {
               skills {
                 title
+                autore
+                motto
+                image{childImageSharp{
+                  gatsbyImageData(
+                        width: 1728
+                        placeholder: BLURRED)
+                }}
                 apps {
                   link
                   name
@@ -20,12 +29,20 @@ const HeaderHome = () => {
                           gatsbyImageData(
                               width:50
                               placeholder: TRACED_SVG
-                              quality: 50)
+                              quality: 90)
                       }
                   }
                 }
               }
               personal {
+                defaultImage{
+                  childImageSharp {
+                    gatsbyImageData(
+                        width: 1728
+                        quality: 90
+                        placeholder: BLURRED)
+                  }
+                }
                 logo {
                   childImageSharp {
                     gatsbyImageData(
@@ -42,64 +59,113 @@ const HeaderHome = () => {
     `)
 
 
-    const skills = data.allDataYaml.edges[0].node.skills
-    const personal = data.allDataYaml.edges[0].node.personal
-    const logo = getImage(personal.logo)
+  const skills = data.allDataYaml.edges[0].node.skills
+  const personal = data.allDataYaml.edges[0].node.personal
+  const logo = getImage(personal.logo)
+  const defaultImage = getImage(personal.defaultImage)
+  const arrayListaApps = []
+  const [app, setApp] = React.useState({})
+  const [mouseOut, setMouseOut] = React.useState(false)
+  const backgrounds = useRef()
 
-    const arrayListaApps = []
-
-    skills.forEach((itema) => {
-        itema.apps.forEach((itemb) => {
-            itemb.main = itema.title
-            return arrayListaApps.push(itemb)
-        })
+  skills.forEach((itema) => {
+    itema.apps.forEach((itemb) => {
+      itemb.main = itema.title
+      arrayListaApps.push(itemb)
     })
-    const [app, setApp] = React.useState()
-    const [mouseOut, setMouseOut] = React.useState(false)
+  })
 
-
-    return (
+  return (
     <header>
-        <div className="block-home">
-            <div className="logo">
-                <GatsbyImage className="image-logo" image={logo} alt={personal.altlogo} />
-                <ul className="titles">
-                    {personal.titles.map((item) => {
-                        return <li>{item}</li>
-                    })}
-                </ul>
-            </div>
-            <ul className="skills">
-                {skills.map((item) => {
-                    return <li key={item.title} onMouseOver={() => {setMouseOut(true); setApp(item.title)}} onMouseLeave={() => {setMouseOut(false)}}>
-                        {item.title}
-                        <ul>
-                            {item.apps.map((item) => {
-                                return <li key={item.title}><a href={item.link} target="_blank" rel="noopener">{item.name}</a></li>
-                            })}
-                        </ul>
-                    </li>
-                })}
-            </ul>
-            <ul className="apps">
-                {arrayListaApps.map((item) => {
-                    const logoApp = getImage(item.logo)
-                    return (
-                        logoApp && <li className={`${((item.main === app)&& mouseOut) ? "open" : ""}`}> <a href={item.link} target="_blank" rel="noopener"> <GatsbyImage class="logo-apps" image={logoApp} alt={item.name} /></a></li>)
-                })}
-            </ul>
+      <div className="block-home">
+        <div className="logo">
+          <GatsbyImage className="image-logo" image={logo} alt={personal.altlogo} />
+          <ul className="titles">
+            {personal.titles.map((item) => {
+              return <li>{item}</li>
+            })}
+          </ul>
         </div>
-        <Scritte app={app} mouseOut={mouseOut}/>
-        <StaticImage
-            src="../images/back-home.jpg"
-            width={1728}
-            quality={95}
-            formats={["auto", "webp", "avif"]}
-            alt="Immagine di sfondo home"
-            className="immagine-home"
+        <ul className="skills">
+          {skills.map((item) => {
+            return <li key={item.title} onMouseOver={() => {
+              setMouseOut(true); setApp(
+                {
+                  title: item.title,
+                  motto: item.motto,
+                  autore: item.autore
+                })
+            }} onMouseLeave={() => { setMouseOut(false) }}>
+              {item.title}
+              <ul>
+                {item.apps.map((item) => {
+                  return <li key={item.title}><a href={item.link} target="_blank" rel="noopener">{item.name}</a></li>
+                })}
+              </ul>
+            </li>
+          })}
+        </ul>
+        <ul className="apps">
+          {arrayListaApps.map((item) => {
+            const logoApp = getImage(item.logo)
+            console.log(app.title === item.main);
+            
+            return (
+                <motion.li
+                  animate={(app.title === item.main) && mouseOut ? {top:-20} : {top:0}}
+                >
+                  <a href={item.link} target="_blank" rel="noopener">
+                    <GatsbyImage class="logo-apps" image={logoApp} alt={item.name} />
+                  </a>
+                </motion.li>
+           
+            )
+          })}
+        </ul>
+      </div>
+
+      <Scritte app={app} mouseOut={mouseOut} />
+
+      {skills.map((item) => {
+        if ((item.title === app.title) && mouseOut) {
+          const immagineSkill = getImage(item.image)
+
+          return (
+            <AnimatePresence>
+              <motion.div
+                className={`immagine-home `}
+                initial={{ opacity: 0.4 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              >
+                <GatsbyImage
+                  ref={backgrounds}
+                  image={immagineSkill}
+                  alt="Immagine di sfondo home"
+
+                />
+              </motion.div>
+            </AnimatePresence>
+          )
+        }
+
+      }
+
+      )}
+
+      {!mouseOut && <motion.div
+        className={`immagine-home default ${mouseOut ? "open" : ""}`}
+        initial={{ opacity: 0, filter: 'blur(10px)' }}
+        animate={{ opacity: 1, filter: 'blur(0px)' }}
+        exit={{ opacity: 0, filter: 'blur(10px)' }}
+      >
+        <GatsbyImage
+          image={defaultImage}
+          alt="Immagine di sfondo home"
         />
+      </motion.div>}
     </header>
-)
+  )
 }
 
 
--- src/components/scritte.jsx
+++ src/components/scritte.jsx
@@ -3,24 +3,35 @@ import { gsap } from 'gsap'
 import { useRef, useEffect } from 'react'
 function Scritte({ app, mouseOut }) {
     const scritte = useRef()
+    const quotes = useRef()
     const timeline = useRef()
 
     useEffect(() => {
         timeline.current = gsap.timeline({ paused: true, })
         timeline.current
             .add("start")
-            .fromTo(scritte.current, { autoAlpha: .0 }, { autoAlpha: 1,  duration:0.7 })
+            .fromTo(scritte.current, { autoAlpha: .0, right: '-40%' }, { autoAlpha: .6, right: '0%', duration: 0.7 })
+            .fromTo(quotes.current, {autoAlpha: 0, top:30}, {autoAlpha:0.7, top:0, duration: 0.7 }, "<")
             .add("stop")
-    },[])
+    }, [])
 
     useEffect(() => {
-        mouseOut ? timeline.current.play(0) : timeline.current.reverse(0)
-
+        mouseOut ? timeline.current.play(0) : timeline.current.reverse(0) 
     }, [mouseOut])
-console.log(mouseOut);
+
     return (
         <>
-            <div className="scritte-home" ref={scritte}>{app}</div>
+            <div className="scritte-home" >
+                <h2 ref={scritte}>
+                    {app.title}
+                </h2>
+                <div className="quotes" ref={quotes}>
+                    <p dangerouslySetInnerHTML={{ __html: app.motto }} />
+                        
+                        <span>{app.autore}</span>
+                    
+                </div>
+            </div>
         </>
     )
 }
